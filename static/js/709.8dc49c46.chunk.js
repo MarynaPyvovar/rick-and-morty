c4f502d97b99ad16d04fab5d05d512567374829b
MarynaPyvovar <95585386+MarynaPyvovar@users.noreply.github.com>
2023-03-14 11:46:44 +0000

    Deploying to gh-pages from @ MarynaPyvovar/rick-and-morty@b1f0d65302f344e3fd5e5b9892efba888b259ec9 🚀

---
+++ static/js/709.8dc49c46.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrick_and_morty=self.webpackChunkrick_and_morty||[]).push([[709],{2709:function(n,r,s){s.r(r);var c=s(8034),e=s(184);r.default=function(){return(0,e.jsxs)("div",{children:[(0,e.jsx)(c.TR,{}),(0,e.jsx)(c.wn,{}),(0,e.jsx)(c.oy,{})]})}}}]);
+//# sourceMappingURL=709.8dc49c46.chunk.js.map
