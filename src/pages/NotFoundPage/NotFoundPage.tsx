d0bae982bf2c8ed3e0234441d5107a3422294c33
MarynaPyvovar <marina18renkas@gmail.com>
2023-03-16 13:13:53 +0200

    add NotFoundPage styles

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import st from 'pages/NotFoundPage/NotFoundPage.module.scss';
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div>
-      NotFoundPage
-    </div>
+    <>
+      <h1 className={st.text}>We don't have any page in this directory :(
+        <br />
+        Please, write the correct one</h1>
+    </>
   )
 }
 
